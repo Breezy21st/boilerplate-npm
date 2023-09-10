a3ec86620278a8311ae849be978a17c4227662f3
Breezy21st <110764642+Breezy21st@users.noreply.github.com>
2023-09-10 17:14:47 +0200

    modified:   myApp.js

--- myApp.js
+++ myApp.js
@@ -14,7 +14,7 @@ app.get("/json", (req, res) => {
     
     if ( process.env.MESSAGE_STYLE === 'uppercase'){
 
-        res.json({ message: "Hello json".toUpperCase()});
+        res.json({ message: "HELLO JSON"});
     } else {
         res.json({ message: "Hello json" });
     }
