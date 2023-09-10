3e790422027c238c5c47ba297cdf28f22801efc1
Breezy21st <110764642+Breezy21st@users.noreply.github.com>
2023-09-10 13:25:30 +0200

    modified:   myApp.js

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ let app = express();
 
 app.get("/", (req, res) => {
     
-    res.sendFile(absolutePath = __dirname + '/view/index.html');
+    res.sendFile(absolutePath = __dirname + '/views/index.html');
 });
 
 console.log("Hello World");
