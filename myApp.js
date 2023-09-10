b525ffab91752a4f53de6850a6bd367e97ff5e78
Breezy21st <110764642+Breezy21st@users.noreply.github.com>
2023-09-10 17:05:20 +0200

    modified:   myApp.js

--- myApp.js
+++ myApp.js
@@ -10,12 +10,12 @@ app.get("/", (req, res) => {
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/json", (req, res) => {
-    const messageStyle = process.env.MESSAGE_STYLE || 'uppercase';
-    
-    if ( messageStyle === 'uppercase'){
 
-        res.json({ message: "HELLO JSON"})
-    } else{
+    
+    if ( process.env.MESSAGE_STYLE === 'uppercase'){
+
+        res.json({ message: "HELLO JSON".toUpperCase()});
+    } else {
         res.json({ message: "Hello json" });
     }
 });
